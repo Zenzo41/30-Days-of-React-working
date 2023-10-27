93f0dc8a3424bda4a0e7d54718a298e8e18ba5f8
Zenzo41 <syangbo.sonam88@gmail.com>
2023-10-27 20:39:58 +0545

    array slice method demonstration

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 const numbers =[1,2,3,4,5,6];
 
 a = numbers.slice(1,4);
+b =numbers.slice(0);
+c =numbers.slice(0,2);
+d =numbers.slice();
+
 console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
