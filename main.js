47ae2810c4a634b9287ddae5cd9db759c9911f0f
Zenzo41 <syangbo.sonam88@gmail.com>
2023-10-28 21:47:31 +0545

    for loop to get sum of even numbers

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
-//ternary operators
+// for loop
 
-let isRaining = true;
-isRaining
-? console.log('It is raining')
-: console.log('It is not raining')
+/*
+for (initialization,condition,increment/decrement)
+{
+    code goes here
+}
+*/
+
+for (let i =0; i<6;i++)
+{
+    console.log(i);
+}
+
+//sum of all numbers from 0 to 100
+
+let sum=0;
+for(let i=0;i<101;i++){
+    sum +=i;
+}
+console.log("The sum of numbers from 1 to 100 is: "+sum);
+
+//sum of even numbers from 1 to 100
+
+let total = 0;
+for(let i=0;i<101;i++)
+{
+    if(i%2==0)
+    {
+        total +=i;
+    }
+}
+console.log(total);
