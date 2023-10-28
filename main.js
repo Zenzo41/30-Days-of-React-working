6e06c4167c8a216484eb9d6eb8ac2dbc3553ba97
Zenzo41 <syangbo.sonam88@gmail.com>
2023-10-28 08:34:16 +0545

    array.push() add new item

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
-const fruits = ['apple','banana','cherry','date','fig'];
+const arr = ['item1','item2','item3'];
+arr.push('item4');
 
-console.log(fruits);
-
-console.log(fruits.splice(2,1)); // cherry is removed
-console.log(fruits);
-
-console.log(fruits.splice(1,2,'lemon','kiwi')); // banana and date are replaced
-console.log(fruits);
-
-console.log(fruits.splice(3,0,'grape')); //adding grape w/o removing/replacing any element
-console.log(fruits);
-
-console.log(fruits.splice(4,0,'melon'));
-console.log(fruits);
+console.log(arr);
