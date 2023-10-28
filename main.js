b170dc79fbc313fd0fc6e5bf7991292d32e0198d
Zenzo41 <syangbo.sonam88@gmail.com>
2023-10-28 21:56:09 +0545

    for loop to access the elements of array

--- main.js
+++ main.js
@@ -1,33 +1,5 @@
-// for loop
-
-/*
-for (initialization,condition,increment/decrement)
+const nums =[1,2,3,4,5];
+for(let i=0;i<5;i++)
 {
-    code goes here
+    console.log(nums[i]);
 }
-*/
-
-for (let i =0; i<6;i++)
-{
-    console.log(i);
-}
-
-//sum of all numbers from 0 to 100
-
-let sum=0;
-for(let i=0;i<101;i++){
-    sum +=i;
-}
-console.log("The sum of numbers from 1 to 100 is: "+sum);
-
-//sum of even numbers from 1 to 100
-
-let total = 0;
-for(let i=0;i<101;i++)
-{
-    if(i%2==0)
-    {
-        total +=i;
-    }
-}
-console.log(total);
