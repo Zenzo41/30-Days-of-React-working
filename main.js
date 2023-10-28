1211fb583a21796f5a70d5066f61f6626a2c6be3
Zenzo41 <syangbo.sonam88@gmail.com>
2023-10-28 20:50:16 +0545

    weather if else if condition

--- main.js
+++ main.js
@@ -10,3 +10,15 @@ if(num>0){
 }else{
     console.log(`${num} is not a number`)
 }
+
+
+let weather = 'sunny'
+if (weather === 'rainy') {
+  console.log('You need a rain coat.')
+} else if (weather === 'cloudy') {
+  console.log('It might be cold, you need a jacket.')
+} else if (weather === 'sunny') {
+  console.log('Go out freely.')
+} else {
+  console.log('No need for rain coat.')
+}
