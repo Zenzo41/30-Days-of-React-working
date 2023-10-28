3802c468a4c398e66451389004b2a749f98f10cd
Zenzo41 <syangbo.sonam88@gmail.com>
2023-10-28 21:20:55 +0545

    ternary operator

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
-let num = prompt('Enter number')
-switch (true) {
-  case num > 0:
-    console.log('Number is positive')
-    break
-  case num == 0:
-    console.log('Numbers is zero')
-    break
-  case num < 0:
-    console.log('Number is negative')
-    break
-  default:
-    console.log('Entered value was not a number')
-}
+//ternary operators
+
+let isRaining = true;
+isRaining
+? console.log('It is raining')
+: console.log('It is not raining')
