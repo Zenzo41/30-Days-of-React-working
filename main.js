c38c20d6ee710be0fe04a2d47b367199b36c46b7
Zenzo41 <syangbo.sonam88@gmail.com>
2023-10-28 21:07:39 +0545

    +ve/-ve/0 using switch case

--- main.js
+++ main.js
@@ -1,39 +1,14 @@
-//switch
-
-let dayUserInput = prompt('What day is today?');
-let day = dayUserInput.toLowerCase();
-
-switch(day)
-{
-        case 'sunday':
-            console.log('Sunday');
-            break;
-
-        case 'monday':
-            console.log('Monday');
-            break;
-
-        case 'tuesday':
-            console.log('Tuesday');
-            break;
-
-        case 'wednesday':
-            console.log('Wednesday');
-            break;
-
-        case 'thursday':
-            console.log('Thursday');
-            break;
-        
-        case 'friday':
-            console.log('Friday');
-            break;
-
-        case 'saturday':
-            console.log('Saturday');
-            break;
-
-        default:
-            console.log("It is not a week day");
-            break;
+let num = prompt('Enter number')
+switch (true) {
+  case num > 0:
+    console.log('Number is positive')
+    break
+  case num == 0:
+    console.log('Numbers is zero')
+    break
+  case num < 0:
+    console.log('Number is negative')
+    break
+  default:
+    console.log('Entered value was not a number')
 }
