1f88bcc5c27d5569f478e7d513df28e9dca10a0d
Zenzo41 <syangbo.sonam88@gmail.com>
2023-10-30 22:47:49 +0545

    for in loop for key and value objects

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-const skills = ['proofreading','copywriting','analysis','coding'];
-skills.forEach((skill,i,skills)=>{
-    console.log(`${i+1}. ${skill}`)
-})
+//for in loop
+
+const data={
+    firstName: "Zen",
+    lastName: "Sama",
+    age: 22,
+    country: "Nepal",
+    skills: ['HTML','CSS','JS','django','SQLite','Python']
+};
+
+for(const d in data)
+{
+    console.log(d,data[d]);
+}
