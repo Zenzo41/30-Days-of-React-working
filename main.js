c393aab5dfb75bf0a9b945f7c6fb3663736da39b
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-01 17:45:20 +0545

    local scope

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-//global scope
-
-let a = 'Javascript';//global scope
-let b = 10;//global scope
-
-function GlobalScope()
-{
-    console.log(a,b);//global scope
-    if (true){
-        let a = 'Python';
-        let b = 100
-        console.log(a,b);//local scope
-    }
-    console.log(a,b);//global scope
+let a = 'JavaScript';
+let b = 10; 
+function letsLearnScope() {
+  console.log(a, b) 
+  let c = 30
+  if (true) {
+    let a = 'Python'
+    let b = 20
+    let d = 40 
+    console.log(a, b, c) 
+  }
+  console.log(a, b) 
 }
-
-GlobalScope();
-console.log(a,b);//global scope
+letsLearnScope()
+console.log(a, b)
