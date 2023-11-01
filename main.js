11cefbc471a3ee72f43a0199d13f1abbb8049565
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-01 09:34:24 +0545

    global scope

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-//window scope
-a = 'JAVASCRIPT';
-b = 100;
-function LearnScope(a,b){
-    console.log(a,b);
-    if(true){
-        console.log(a,b);
+//global scope
+
+let a = 'Javascript';//global scope
+let b = 10;//global scope
+
+function GlobalScope()
+{
+    console.log(a,b);//global scope
+    if (true){
+        let a = 'Python';
+        let b = 100
+        console.log(a,b);//local scope
     }
+    console.log(a,b);//global scope
 }
-console.log(a,b);
+
+GlobalScope();
+console.log(a,b);//global scope
