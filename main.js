4d7a47509a74fc8f8685b7dfe53ae1afdcf4e897
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-02 12:43:47 +0545

    getFullName

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 const person = {
-  firstName: 'Zen',
-  lastName: 'Sama',
-  age:22,
-  skills:['HTML','CSS','Javascript'],
-  isMarried: true
-}
-console.log(person.skills);
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+  age: 250,
+  country: 'Finland',
+  city: 'Helsinki',
+  skills: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Node',
+    'MongoDB',
+    'Python',
+    'D3.js',
+  ],
+  getFullName: function () {
+    return `${person.firstName} ${person.lastName}`; // Added a space between the names
+  },
+  'phone number': '+3584545454545',
+};
+
+const fullName = person.getFullName(); // Now it will work
+console.log(fullName); // Output: Asabeneh Yetayeh
