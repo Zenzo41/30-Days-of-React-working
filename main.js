1bc03649577a0a446cd607f4609a9daa3a2bb927
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-03 00:04:00 +0545

    Object method: assign

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-const person = {
-  firstName:"Zen",
-  lastName: "Sama",
-  skills: {
-    coding: true,
-    designing: false,
-    photography: true,
-  },
-  getPersonInfo: function (){
-    return `I am ${this.firstName} ${this.lastName} and I do photography: ${person.skills.photography}`;
-  },
-}
-
-const thisPerson = person.getPersonInfo();
-console.log(thisPerson);
+const objec = {key:'value'};
+const person = Object.assign({},objec);
+console.log(person.key);
