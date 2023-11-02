6b119cc542050b886bad3dfcb50c0e7c914b4f6e
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-03 00:24:50 +0545

    assign,keys and value of Object

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-const objec = {key:'value'};
-const person = Object.assign({},objec);
-console.log(person.key);
+const person = {
+  firstName: 'Asabeneh',
+  age: 250,
+  country: 'Finland',
+  city: 'Helsinki',
+  skills: ['HTML', 'CSS', 'JS'],
+  title: 'teacher',
+  address: {
+    street: 'Heitamienkatu 16',
+    pobox: 2002,
+    city: 'Helsinki',
+  },
+  getPersonInfo: function () {
+    return `I am ${this.firstName} and I live in ${this.city}, ${this.country}. I am ${this.age}.`
+  },
+}
+
+const copyPerson = Object.assign({}, person)
+console.log(copyPerson)
+
+const keys = Object.keys(copyPerson)
+console.log(keys) //['name', 'age', 'country', 'skills', 'address', 'getPersonInfo']
+const address = Object.keys(copyPerson.address)
+console.log(address) //['street', 'pobox', 'city']
+
+const values = Object.values(copyPerson)
+console.log(values)
