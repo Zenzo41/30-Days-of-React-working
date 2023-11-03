a51d137cef2d7bbb190c589f7d6b3ca204a1ca2a
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-03 13:39:24 +0545

    upto self invoking function

--- main.js
+++ main.js
@@ -1,14 +1,43 @@
-// function with many parameter
+// Function with unlimited number of parameters
 
-function sumArrayValues(arr)
+function sumAllNums()
 {
     let sum = 0;
-    for (let i = 0; i<arr.length;i++)
+    for(let i=0; i<arguments.length; i++)
     {
-        sum = sum+arr[i];
+        sum += arguments[i];
     }
     return sum;
 }
 
-const numbers = [1,2,3,4,5];
-console.log("Sum of array values: "+sumArrayValues(numbers));
+console.log(sumAllNums(1,2,3,5));
+
+//arrow function
+
+const arrowSum = (...args) => { // ... parameter followed by spread operator
+    console.log(args)
+}
+
+arrowSum(2,3,4,5,44);
+
+//Anonymous function or without name
+
+const anonymousFun = function () {
+  console.log(
+    'I am an anonymous function and my value is stored in anonymousFun'
+  )
+}
+console.log(anonymousFun);
+
+//Expression Function
+
+const square = function (n) {
+    return n*n;
+}
+console.log(square(3));
+
+//Self invoking function
+let squaredNum = function (n) {
+    return n*n;
+}
+console.log(squaredNum(22));
