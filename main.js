0813289f870f4bdbfb9cf855e5f7c3e3fbcec000
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-03 09:11:32 +0545

    function returning value

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-function square()
-{
-    let num = 2 ;
-    let sq = num * num;
-    console.log(sq);
-}
+// function returning value
 
-square();
+function printFullName()
+{
+    let firstName = 'Zen';
+    let lastName = 'Sama';
+    let space =' ';
+    let fullName = firstName+space+lastName;
+    return fullName;
+}
+console.log(printFullName());
+
+function addTwoNumbers() {
+    let numOne = 2
+    let numTwo = 3
+    let total = numOne + numTwo
+    return total
+  }
+  
+  console.log(addTwoNumbers())
