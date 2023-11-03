e72e2192f77755fff5ec68d3a4ac8375fd19f6e8
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-03 19:06:24 +0545

    upperCase

--- main.js
+++ main.js
@@ -1,43 +1,12 @@
-// Function with unlimited number of parameters
-
-function sumAllNums()
-{
-    let sum = 0;
-    for(let i=0; i<arguments.length; i++)
+const changeToUpperCase = (arr)=>{
+    const newArr = []
+    for (const element of arr)
     {
-        sum += arguments[i];
+        newArr.push(element.toUpperCase())
     }
-    return sum;
+    return newArr
 }
 
-console.log(sumAllNums(1,2,3,5));
-
-//arrow function
-
-const arrowSum = (...args) => { // ... parameter followed by spread operator
-    console.log(args)
-}
-
-arrowSum(2,3,4,5,44);
-
-//Anonymous function or without name
-
-const anonymousFun = function () {
-  console.log(
-    'I am an anonymous function and my value is stored in anonymousFun'
-  )
-}
-console.log(anonymousFun);
-
-//Expression Function
-
-const square = function (n) {
-    return n*n;
-}
-console.log(square(3));
-
-//Self invoking function
-let squaredNum = function (n) {
-    return n*n;
-}
-console.log(squaredNum(22));
+const inputArray = ['Sonam'];
+const result = changeToUpperCase(inputArray);
+console.log(result);
