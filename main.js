e8c6f463c11148016d6cbcbfce9cd10fc43f650e
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-03 09:04:17 +0545

    function without parameter

--- main.js
+++ main.js
@@ -1,22 +1,8 @@
-const person = {
-  firstName: 'Asabeneh',
-  age: 250,
-  country: 'Finland',
-  city: 'Helsinki',
-  skills: ['HTML', 'CSS', 'JS'],
-  title: 'teacher',
-  address: {
-    street: 'Heitamienkatu 16',
-    pobox: 2002,
-    city: 'Helsinki',
-  },
-  getPersonInfo: function () {
-    return `I am ${this.firstName} and I live in ${this.city}, ${this.country}. I am ${this.age}.`
-  },
+function square()
+{
+    let num = 2 ;
+    let sq = num * num;
+    console.log(sq);
 }
 
-const copyPerson = Object.assign({}, person)
-console.log(copyPerson)
-
-const entries = Object.entries(copyPerson)
-console.log(entries)
+square();
