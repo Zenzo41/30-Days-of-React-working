2a91e5f51cd4e9dc69b4eb909ee20e5cb4c01ff3
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-04 23:03:08 +0545

    setting default parameter

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-//Destructuring arrays
-
-const numbers = [1,2,3];
-const [n1,n2,n3] = numbers;
-console.log(n1,n2,n3);
-
-const constants = [2.72, 3.14, 9.81,37, 100]
-const [e, pi, gravity, bodyTemp, boilingTemp] = constants
-console.log(e, pi, gravity, bodyTemp, boilingTemp)
-
-const countries = ['Finland', 'Sweden', 'Norway']
-const [fin, swe, nor] = countries
-console.log(fin, swe, nor)
+const countries = ['Finland', 'Sweden', undefined, 'Norway']
+const [fin, swe, ice = 'Iceland', nor, den = 'Denmark'] = countries
+console.log(fin, swe, ice, nor, den)
