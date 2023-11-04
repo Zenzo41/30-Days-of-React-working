97826666df4dc3638a2b90d7678013c0be04cbb0
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-04 23:11:40 +0545

    nested arr,destructuring,forEach

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-const countries = ['Finland', 'Sweden', undefined, 'Norway']
-const [fin, swe, ice = 'Iceland', nor, den = 'Denmark'] = countries
-console.log(fin, swe, ice, nor, den)
+//Destructuring Nested Arrats
+
+const fullStack = [
+    ['HTML','CSS','JS','React'],
+    ['Node','Express','MongoDB']
+];
+
+const [frontEnd,backEnd] = fullStack;
+console.log(frontEnd,backEnd);
+
+frontEnd.forEach((item,i,frontEnd)=>{
+    console.log(`${i+1}. ${item}`);
+});
