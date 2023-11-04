88bfbf7ab42dd8dac023dd32f7a7eb8f94cf13be
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-04 21:12:08 +0545

    forEach loop

--- main.js
+++ main.js
@@ -2,10 +2,6 @@
 numbers = [1,2,3,4,5];
 countries = ['Finland','Nepal','USA','UK'];
 
-for (const number of numbers){
-    console.log(number);
-}
-
-for(const country of countries){
-    console.log(country);
-}
+countries.forEach((item,i,countries) => {
+    console.log(`${i+1}. ${item}`)
+});
