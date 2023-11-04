b790883f347d3cbd2edf99a4d27234a5ce4461d7
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-04 20:31:15 +0545

    using forEach for the sumofArray

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 const numbers = [1,2,3,4];
-const sumArray = arr => {
-    let sum = 0;
-    const callback = function(element){
+
+const sumArray = arr =>{
+    let sum = 0 ;
+    arr.forEach(function(element){
         sum += element;
-    }
-    arr.forEach(callback)
-    {
-        return sum
-    }
+    })
+    return sum;
 }
 
 console.log(sumArray(numbers));
