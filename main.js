292659139395d7b5e15d11f50c93dc42aafebf0e
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-04 21:25:46 +0545

    accessing array items manually

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-//Accessing array items using loop
-numbers = [1,2,3,4,5];
-countries = ['Finland','Nepal','USA','UK'];
+// Accessing array items manually
 
-countries.forEach((item,i,countries) => {
-    console.log(`${i+1}. ${item}`)
-});
+const numbers = [1,2,3];
+let num1 = numbers[0];
+let num2 = numbers[1];
+let num3 = numbers[2];
+
+console.log(num1,num2,num3);
+
+const countries = ['Finland','Sweden','Norway'];
+let country1 = countries[0];
+let country2 = countries[1];
+let country3 = countries[2];
+
+console.log(country1,country2,country3);
