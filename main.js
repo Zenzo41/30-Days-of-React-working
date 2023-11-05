1a9ddae21c393202dfa788e76cc4b7ed5a568e11
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-05 14:54:35 +0545

    useState hook

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-const countries = [
-    'Germany',
-    'France',
-    'Belgium',
-    'Finland',
-    'Sweden',
-    'Norway',
-    'Denmark',
-    'Iceland',
-  ]
+// Destructuring when we loop through arrays
 
-const [c1,c2,c3,...rest] = countries;
-console.log(c1,c2,c3,rest);
+const fullStack = [
+    ['HTML','CSS','JS','React'],
+    ['Node','Express','MongoDB'],
+]
+
+for(const [first,second,third,fourth="That's it"] of fullStack)
+{
+    console.log(first,second,third,fourth)
+}
+
+//React Hooks useState hook?
+const [x,y] =[2,(value) => value **2 ]
+console.log(x);
