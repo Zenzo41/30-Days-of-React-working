287341b435025887fb05e31d6101ed40dff4aa5f
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-05 09:33:47 +0545

    using spread operator

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-//getting rest of array using spread operator
+const countries = [
+    'Germany',
+    'France',
+    'Belgium',
+    'Finland',
+    'Sweden',
+    'Norway',
+    'Denmark',
+    'Iceland',
+  ]
 
-const nums = [1,2,3,4,5,6,7,8,9,10];
-
-const [num1,num2,num3,...rest] = nums;
-console.log(num1,num2,num3,rest);
+const [c1,c2,c3,...rest] = countries;
+console.log(c1,c2,c3,rest);
