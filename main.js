6198add45dec20c7abcafd5c6b8e630314ec2f4f
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-06 09:30:16 +0545

    Spread operator with arrow function

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 const sumAllNums = (...args) => {
-    console.log(args)
-}
+    let sum = 0
+    for (const num of args) {
+      sum += num
+    }
+    return sum
+  }
+  
+  console.log(sumAllNums(1, 2, 3, 4, 5))
