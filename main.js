90ddaf26b38be20e8e45a269d47b4ade2d360e51
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-06 10:03:48 +0545

    forEach method

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-const sumAllNums = (...args) => {
-    let sum = 0
-    for (const num of args) {
-      sum += num
-    }
-    return sum
-  }
-  
-  console.log(sumAllNums(1, 2, 3, 4, 5))
+const countries = ['India','UK','USA']
+countries.forEach((item,i,countries) => {
+    console.log(`${i+1}. ${item}`);    
+});
