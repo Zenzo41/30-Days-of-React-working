8f3f5dfc376636907caf94920b9e0a8eff2efe05
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-07 11:24:03 +0545

    evens and odds using filter

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const countries = ['Finland', 'Estonia', 'Sweden', 'Norway', 'Iceland'];
-const countriesWithLand = countries.filter((country) => {
-    return country.includes('land');
-});
-console.log(countriesWithLand);
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const evens = numbers.filter((n)=> n%2===0);
+const odds = numbers.filter((n)=> n%2 !==0);
+
+console.log(evens);
+console.log(odds);
