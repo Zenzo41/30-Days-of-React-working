5c1ac2e76af153c9271fd9933eb82487b26bdc02
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-07 21:44:30 +0545

    reduce method

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+//reduce
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const evens = numbers.filter((n)=> n%2===0);
-const odds = numbers.filter((n)=> n%2 !==0);
+const sum = numbers.reduce((storage,currentElement)=> storage+currentElement)
 
-console.log(evens);
-console.log(odds);
+console.log(sum);
+
+const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const sum1 = values.reduce((acc, cur) => acc + cur, 5)
+console.log(sum1) 
