813c1efaa57064d3199ba0986cdf0f34a939bc1b
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-08 11:49:40 +0545

    constructor

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-class Person{
-    //code goes here
-}
-
-const person = new Person()
-console.log(person)
+class Person {
+    constructor(firstName, lastName) {
+      console.log(this) // Check the output from here
+      this.firstName = firstName
+      this.lastName = lastName
+    }
+  }
+  
+  const person = new Person()
+  
+  console.log(person)
