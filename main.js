269f69d5965440219e36d78b6b521b9698b743a7
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-08 09:07:21 +0545

    concatenated string

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-//reduce
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const sum = numbers.reduce((storage,currentElement)=> storage+currentElement)
-
-console.log(sum);
-
-const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const sum1 = values.reduce((acc, cur) => acc + cur, 5)
-console.log(sum1) 
+const  words = ['Hello','','World','!'];
+const concatenatedString = words.reduce((accum,current)=> accum+current+' ');
+console.log(concatenatedString);
