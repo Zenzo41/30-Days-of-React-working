421a4aed03922ba15f4307ca2a5d0fe064a9e274
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-08 18:36:48 +0545

    default values with constructor

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-class Person{
-    constructor(firstname,lastname,age,country,city)
-    {
-        this.firstname = firstname
-        this.lastname= lastname;
-        this.age = age;
-        this.country = country;
-        this.city = city;
+class Person {
+    constructor(
+      firstName = 'Asabeneh',
+      lastName = 'Yetayeh',
+      age = 250,
+      country = 'Finland',
+      city = 'Helsinki'
+    ) {
+      this.firstName = firstName
+      this.lastName = lastName
+      this.age = age
+      this.country = country
+      this.city = city
     }
-}
-
-const person1 = new Person('Zen','Sama',22,'India','New Delhi');
-console.log(person1);
-console.log(person1.firstname);
-console.log(person1.lastname);
-console.log(person1.age);
-console.log(person1.country);
-console.log(person1.city);
+  }
+  
+  const person1 = new Person() // it will take the default values
+  const person2 = new Person('Lidiya', 'Tekle', 28, 'Finland', 'Espoo')
+  
+  console.log(person1)
+  console.log(person2)
