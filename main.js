661f5d48d0b3ac436cc41fab54b08d243275ed55
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-08 11:10:02 +0545

    class instantiation

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const someAreEvens = numbers.every((n) => n % 2 === 0)
-const someAreOdds = numbers.every((n) => n % 2 !== 0)
-console.log(someAreEvens)
-console.log(someAreOdds)
+class Person{
+    //code goes here
+}
+
+const person = new Person()
+console.log(person)
