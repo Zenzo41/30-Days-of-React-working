338e3a378eff2460cdc18ac79f643af8ca9e5d4e
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-08 18:34:15 +0545

    adding other properties

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-class Person {
-    constructor(firstName, lastName) {
-      console.log(this) // Check the output from here
-      this.firstName = firstName
-      this.lastName = lastName
+class Person{
+    constructor(firstname,lastname,age,country,city)
+    {
+        this.firstname = firstname
+        this.lastname= lastname;
+        this.age = age;
+        this.country = country;
+        this.city = city;
     }
-  }
-  
-const person1 = new Person("Zen","Sama");
-const person2 = new Person("Ab","Sama");
-const person3 = new Person("asca","Sama");
+}
 
+const person1 = new Person('Zen','Sama',22,'India','New Delhi');
 console.log(person1);
-console.log(person1);
-console.log(person1);
+console.log(person1.firstname);
+console.log(person1.lastname);
+console.log(person1.age);
+console.log(person1.country);
+console.log(person1.city);
