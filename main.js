1b349e922d05b7dfa2466cbabe50d5b2490b99d5
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-08 10:35:43 +0545

    some methd to return boolean values

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-const countries = ['Finland', 'Estonia', 'Sweden', 'Norway', 'Iceland']
-const index = countries.findIndex((country) => country.includes('o'))
-console.log(index)
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const someAreEvens = numbers.some((n) => n % 2 === 0)
+const someAreOdds = numbers.some((n) => n % 2 !== 0)
+console.log(someAreEvens)
+console.log(someAreOdds)
