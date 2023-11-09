7e1a8a0d909724b0b56e60fa4ccb0e0382d4a38c
Zenzo41 <syangbo.sonam88@gmail.com>
2023-11-09 08:51:56 +0545

    using get and set

--- main.js
+++ main.js
@@ -1,45 +1,49 @@
 class Person {
-    constructor(firstName, lastName, age, country, city) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.age = age;
-      this.country = country;
-      this.city = city;
-      this.score = 0;
-      this.skills = [];
-    }
-  
-    getFullName() {
-      const fullName = this.firstName + ' ' + this.lastName;
-      return fullName;
-    }
-  
-    get getScore() {
-      return this.score;
-    }
-  
-    get getSkills() {
-      return this.skills;
-    }
-  
-    set setScore(score) {
-      this.score += score;
-    }
-  
-    set setSkill(skill) {
-      this.skills.push(skill);
-    }
+  constructor(firstName, lastName, age, country, city) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.country = country
+    this.city = city
+    this.score = 0
+    this.skills = []
   }
-  
-  const p1 = new Person('Zen','Sama',22,'Norway','Ottoman');
-  p1.setScore = 1;
-  p1.setSkill = 'HTML';
-  p1.setSkill = 'CSS';
-  p1.setSkill = 'Javascript';
+  getFullName() {
+    const fullName = this.firstName + ' ' + this.lastName
+    return fullName
+  }
+  get getScore() {
+    return this.score
+  }
+  get getSkills() {
+    return this.skills
+  }
+  set setScore(score) {
+    this.score += score
+  }
+  set setSkill(skill) {
+    this.skills.push(skill)
+  }
+  getPersonInfo() {
+    let fullName = this.getFullName()
+    let skills =
+      this.skills.length > 0 &&
+      this.skills.slice(0, this.skills.length - 1).join(', ') +
+        ` and ${this.skills[this.skills.length - 1]}`
+    let formattedSkills = skills ? `He knows ${skills}` : ''
 
-  console.log(p1.getScore);
-  console.log(p1.getSkills[0]);
-  console.log(p1.getSkills[1]);
-  console.log(p1.getSkills[2]);
-  
-  
+    let info = `${fullName} is ${this.age}. He lives ${this.city}, ${this.country}. ${formattedSkills}`
+    return info
+  }
+}
+
+const P1 = new Person('zen','Sama',22,'inISAS','JISDHF');
+P1.setScore = 90;
+P1.setSkill='Sjkjhasd';
+P1.setSkill='sdjksd';
+P1.setSkill='sdhic';
+
+console.log(P1.getFullName);
+console.log(P1.getPersonInfo);
+console.log(P1.getScore);
+console.log(P1.getSkills);
